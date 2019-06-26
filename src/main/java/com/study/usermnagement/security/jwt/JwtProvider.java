c91c2b008199456b93d20503d3bcede6de2be15a
Hoàng Tháp <thap1>
2019-06-26 23:58:21 +0700

    26/6

--- src/main/java/com/study/usermnagement/security/jwt/JwtProvider.java
+++ src/main/java/com/study/usermnagement/security/jwt/JwtProvider.java
@@ -1,5 +1,62 @@
 package com.study.usermnagement.security.jwt;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import com.study.usermnagement.security.services.UserPrinciple;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+@Component
 public class JwtProvider {
 
+	private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+
+	@Value("${grokonez.app.jwtSecret}")
+	private String jwtSecret;
+
+	@Value("${grokonez.app.jwtExpiration}")
+	private int jwtExpriration;
+
+	public String genarateJwtToken(Authentication authentication) {
+		UserPrinciple userPrincial = (UserPrinciple) authentication.getPrincipal();
+		return Jwts.builder().setSubject(userPrincial.getUsername()).setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime() + jwtExpriration * 1000))
+				.signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+	}
+
+	public boolean validateJwtToken(String authtoken) {
+		try {
+			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authtoken);
+			return true;
+		} catch (
+
+		SignatureException e) {
+			logger.error("Invalid JWT signature -> Message: {} ", e);
+		} catch (MalformedJwtException e) {
+			logger.error("Invalid JWT token -> Message: {}", e);
+		} catch (ExpiredJwtException e) {
+			logger.error("Expired JWT token -> Message: {}", e);
+		} catch (UnsupportedJwtException e) {
+			logger.error("Unsupported JWT token -> Message: {}", e);
+		} catch (IllegalArgumentException e) {
+			logger.error("JWT claims string is empty -> Message: {}", e);
+		}
+
+		return false;
+	}
+
+	public String getUserNameFromJwtToken(String token) {
+		return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+	}
 }
--- src/main/java/com/study/usermnagement/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/study/usermnagement/security/services/UserDetailsServiceImpl.java
@@ -1,5 +1,30 @@
 package com.study.usermnagement.security.services;
 
-public class UserDetailsServiceImpl {
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.study.usermnagement.model.User;
+import com.study.usermnagement.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		User user = userRepository.findByUsername(username).orElseThrow(
+				() -> new UsernameNotFoundException("User not Found with -> username or email: " + username));
+
+		return UserPrinciple.build(user);
+	}
 
 }
